74ce0112c98d81e4ce3cf2fa0e83e67731386603
Ulises Castillo <uac@fb.com>
2023-10-10 21:54:52 -0700

    Initial Commit

---
+++ WeGym/WeGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeGymApp.swift
+//  WeGym
+//
+//  Created by Ulises Castillo on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
