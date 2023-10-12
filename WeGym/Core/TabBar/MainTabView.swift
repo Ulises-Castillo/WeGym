6fd629c2bef4e5c4f241fe4570b5795e09ed25f6
Ulises Castillo <uac@fb.com>
2023-10-12 15:56:07 -0700

    Setup MainTabView

---
+++ WeGym/Core/TabBar/MainTabView.swift
@@ -0,0 +1,39 @@
+//
+//  MainTabView.swift
+//  WeGym
+//
+//  Created by Ulises Castillo on 10/12/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+      TabView {
+        Text("Home")
+          .tabItem {
+            Image(systemName: "house")
+          }
+        
+        Text("Search")
+          .tabItem {
+            Image(systemName: "magnifyingglass")
+          }
+        
+        Text("Notifications")
+          .tabItem {
+            Image(systemName: "person.bubble")
+          }
+        
+        Text("Profile")
+          .tabItem {
+            Image(systemName: "person")
+          }
+      }
+      .accentColor(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
