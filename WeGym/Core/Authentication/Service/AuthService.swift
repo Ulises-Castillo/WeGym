7c1baf9fdcf0ae5513700172e74fe8b2757542bb
Ulises Castillo <uac@fb.com>
2023-10-13 16:58:04 -0700

    Added user data upload

--- WeGym/Core/Authentication/Service/AuthService.swift
+++ WeGym/Core/Authentication/Service/AuthService.swift
@@ -6,7 +6,9 @@
 //
 
 import Foundation
+import Firebase
 import FirebaseAuth
+import FirebaseFirestoreSwift
 
 class AuthService {
   
@@ -33,6 +35,9 @@ class AuthService {
     do {
       let result = try await Auth.auth().createUser(withEmail: email, password: password)
       userSession = result.user
+      print("DEBUG: Did create user..") //TODO: add debug logger
+      await uploadUserData(uid: result.user.uid, username: username, email: email)
+      print("DEBUG: Did upload user data..")
     } catch {
       print("DEBUG: Failed to register user with error: \(error.localizedDescription)")
     }
@@ -45,5 +50,13 @@ class AuthService {
   func signOut() {
     try? Auth.auth().signOut() //TODO: handle failure/optional
     userSession = nil
+  }
+  
+  private func uploadUserData(uid: String, username: String, email: String) async {
+    let user = User(id: uid, email: email, username: username)
+    guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+    
+    //FIXME: don't hardcode "users" path string // separate file with constants
+    try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
   }
 }
