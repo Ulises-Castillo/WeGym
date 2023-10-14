3309b53d17b21bcc430a9e64fb3eaeaf82ed5ea2
Ulises Castillo <uac@fb.com>
2023-10-13 17:58:34 -0700

    Added fetch all users

---
+++ WeGym/Services/UserService.swift
@@ -0,0 +1,17 @@
+//
+//  UserService.swift
+//  WeGym
+//
+//  Created by Ulises Castillo on 10/13/23.
+//
+
+import Foundation
+import Firebase
+
+struct UserService  {
+  
+  static func fetchAllUsers() async throws -> [User] {
+    let snapshot = try await Firestore.firestore().collection("users").getDocuments()
+    return snapshot.documents.compactMap({ try? $0.data(as: User.self) })
+  }
+}
