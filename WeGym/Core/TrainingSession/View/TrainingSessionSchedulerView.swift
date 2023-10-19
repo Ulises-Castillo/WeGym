6611862aa313f53f91db0586b4b4142055371a11
Ulises Castillo <uac@fb.com>
2023-10-19 16:50:00 -0700

    Increase caption padding when keyboard shown

--- WeGym/Core/TrainingSession/View/TrainingSessionSchedulerView.swift
+++ WeGym/Core/TrainingSession/View/TrainingSessionSchedulerView.swift
@@ -103,6 +103,7 @@ struct TrainingSessionSchedulerView: View {
           .padding()
         }
       }
+      .keyboardAvoiding()
       .foregroundColor(.primary)
       .navigationTitle(viewModel.currentUserTrainingSesssion == nil ? "Add Workout" : "Edit Workout")
       .navigationBarTitleDisplayMode(.inline)
@@ -201,4 +202,42 @@ extension Date {
     }
     return date - TimeInterval(minutes)*60 - TimeInterval(seconds)
   }
+}
+
+public extension Publishers {
+    static var keyboardHeight: AnyPublisher<CGFloat, Never> {
+        let willShow = NotificationCenter.default.publisher(for: UIApplication.keyboardWillShowNotification)
+            .map { $0.keyboardHeight }
+        let willHide = NotificationCenter.default.publisher(for: UIApplication.keyboardWillHideNotification)
+            .map { _ in CGFloat(0) }
+
+        return MergeMany(willShow, willHide)
+            .eraseToAnyPublisher()
+    }
+}
+
+public extension Notification {
+    var keyboardHeight: CGFloat {
+        return (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect)?.height ?? 0
+    }
+}
+
+public struct KeyboardAvoiding: ViewModifier {
+    @State private var keyboardActiveAdjustment: CGFloat = 0
+
+    public func body(content: Content) -> some View {
+        content
+            .safeAreaInset(edge: .bottom, spacing: keyboardActiveAdjustment) {
+                EmptyView().frame(height: 0)
+            }
+            .onReceive(Publishers.keyboardHeight) {
+                self.keyboardActiveAdjustment = min($0, 66) // keyboard padding
+            }
+    }
+}
+
+public extension View {
+    func keyboardAvoiding() -> some View {
+        modifier(KeyboardAvoiding())
+    }
 }
