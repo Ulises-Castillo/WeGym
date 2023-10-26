6e604fb6466204f38b1238732a2ba0f65834dca4
Ulises Castillo <uac@fb.com>
2023-10-26 16:03:37 -0700

    Added LPLinkViewRepresented

---
+++ WeGym/Core/Messenger/Components/LinkPreviews/View/LPLinkViewRepresented.swift
@@ -0,0 +1,25 @@
+//
+//  LPLinkViewRepresented.swift
+//  WeGym
+//
+//  Created by Ulises Castillo on 10/26/23.
+//
+
+import LinkPresentation
+import SwiftUI
+
+class CustomLinkView: LPLinkView {
+    override var intrinsicContentSize: CGSize { CGSize(width: 0, height: super.intrinsicContentSize.height) }
+}
+
+struct LPLinkViewRepresented: UIViewRepresentable {
+    typealias UIViewType = CustomLinkView
+    var metadata: LPLinkMetadata
+
+    func makeUIView(context: Context) -> CustomLinkView {
+        let linkView = CustomLinkView(metadata: metadata)
+        return linkView
+    }
+
+    func updateUIView(_ uiView: CustomLinkView, context: Context) {}
+}
