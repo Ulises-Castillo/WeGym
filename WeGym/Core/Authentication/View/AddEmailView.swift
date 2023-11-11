34021189b9cdb15df98da03aeaac7ed72ceba14f
Ulises Castillo <uac@fb.com>
2023-11-10 22:27:22 -0800

    Complete advanced validation for username/password

--- WeGym/Core/Authentication/View/AddEmailView.swift
+++ WeGym/Core/Authentication/View/AddEmailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AddEmailView: View {
   @Environment(\.dismiss) var dismiss
   @EnvironmentObject var viewModel: RegistrationViewModel
+  @State private var showCreateUsernameView = false
   @FocusState var inputFocused: Bool
 
   var body: some View {
@@ -18,27 +19,52 @@ struct AddEmailView: View {
         .font(.title2)
         .fontWeight(.bold)
         .padding(.top)
-      
+
       Text("You'll use this email to sign to your account")
         .font(.footnote)
         .foregroundColor(.gray)
         .multilineTextAlignment(.center)
         .padding(.horizontal, 24)
         .padding(.top, 1)
-      
-      TextField("Email", text: $viewModel.email)
-        .keyboardType(.asciiCapable)
-        .textContentType(.oneTimeCode)
-        .disableAutocorrection(true)
-        .autocapitalization(.none)
-        .autocorrectionDisabled(true)
-        .modifier(WGTextFieldModifier())
-        .padding(.top)
-        .focused($inputFocused)
 
-      NavigationLink {
-        CreateUsernameView()
-          .navigationBarBackButtonHidden()
+      ZStack(alignment: .trailing) {
+        TextField("Email", text: $viewModel.email)
+          .keyboardType(.asciiCapable)
+          .textContentType(.oneTimeCode)
+          .disableAutocorrection(true)
+          .autocapitalization(.none)
+          .autocorrectionDisabled(true)
+          .modifier(WGTextFieldModifier())
+          .padding(.top)
+          .focused($inputFocused)
+
+        if viewModel.isLoading {
+          ProgressView()
+            .padding(.trailing, 40)
+            .padding(.top, 14)
+        }
+
+        if viewModel.emailValidationFailed {
+          Image(systemName: "xmark.circle.fill")
+            .imageScale(.large)
+            .fontWeight(.bold)
+            .foregroundColor(Color(.systemRed))
+            .padding(.trailing, 40)
+            .padding(.top, 14)
+        }
+      }
+
+      if viewModel.emailValidationFailed {
+        Text("This email is already in use.")
+          .font(.caption)
+          .foregroundColor(Color(.systemRed))
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .padding(.horizontal, 28)
+      }
+
+
+      Button {
+        Task { try await viewModel.validateEmail() }
       } label: {
         Text("Next")
           .font(.subheadline)
@@ -51,10 +77,21 @@ struct AddEmailView: View {
       .disabled(!formIsValid)
       .opacity(formIsValid ? 1.0 : 0.5)
       .padding(.vertical)
-      
+
       Spacer()
     }
+    .onReceive(viewModel.$emailIsValid, perform: { emailIsValid in
+      if emailIsValid {
+        self.showCreateUsernameView.toggle()
+      }
+    })
+    .navigationDestination(isPresented: $showCreateUsernameView, destination: {
+      CreateUsernameView()
+        .navigationBarBackButtonHidden()
+    })
     .onAppear {
+      showCreateUsernameView = false
+      viewModel.emailIsValid = false
       inputFocused = true
     }
     .toolbar {
@@ -64,18 +101,18 @@ struct AddEmailView: View {
           .onTapGesture {
             dismiss()
           }
-        
+
       }
     }
   }
 }
 
 extension AddEmailView {
-    var formIsValid: Bool {
-        return !viewModel.email.isEmpty
-        && viewModel.email.contains("@")
-        && viewModel.email.contains(".")
-    }
+  var formIsValid: Bool {
+    return !viewModel.email.isEmpty
+    && viewModel.email.contains("@")
+    && viewModel.email.contains(".")
+  }
 }
 
 #Preview {
