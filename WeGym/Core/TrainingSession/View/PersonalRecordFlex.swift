59a401685dabde01c7bb9e69947956ed8f3581f3
Ulises Castillo <uac@fb.com>
2023-11-11 18:32:26 -0800

    Added lighter/darker color utilities for future feature: distinguish past / future cells

---
+++ WeGym/Core/TrainingSession/View/PersonalRecordFlex.swift
@@ -0,0 +1,18 @@
+//
+//  PersonalRecordFlex.swift
+//  WeGym
+//
+//  Created by Ulises Castillo on 11/11/23.
+//
+
+import SwiftUI
+
+struct PersonalRecordFlex: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PersonalRecordFlex()
+}
