6080e311c5de95af0dc37aee1b44f22b9d7c0998
Ulises Castillo <uac@fb.com>
2023-11-17 21:01:31 -0800

    Extracted User Stats view

--- WeGym/Core/Profile/View/CurrentUserProfileView.swift
+++ WeGym/Core/Profile/View/CurrentUserProfileView.swift
@@ -27,24 +27,7 @@ struct CurrentUserProfileView: View {
 
           //          PostGridView(config: .profile(UserService.shared.currentUser!))
 
-          HStack(spacing: 24) {
-            NavigationLink(value: CurrentUserProfileNavigation.trainingSessions) {
-              UserStatView(value: String(viewModel.user.stats?.trainingSessions ?? 0), title: "Workouts")
-            }
-            .disabled(viewModel.user.stats?.trainingSessions == 0)
-
-            NavigationLink(value: CurrentUserProfileNavigation.followers(viewModel.user.id)) {
-              UserStatView(value: String(viewModel.user.stats?.followers ?? 0), title: "Followers")
-            }
-            .disabled(viewModel.user.stats?.followers == 0)
-
-            NavigationLink(value: CurrentUserProfileNavigation.following(viewModel.user.id)) {
-              UserStatView(value: String(viewModel.user.stats?.following ?? 0), title: "Following")
-            }
-            .disabled(viewModel.user.stats?.following == 0)
-          }
-          .padding(.trailing)
-          .foregroundColor(.primary)
+          UserStatsHStackView(viewModel: viewModel)
         }
       }
       .navigationTitle(UserService.shared.currentUser?.username ?? "")
--- WeGym/Core/Profile/View/ProfileView.swift
+++ WeGym/Core/Profile/View/ProfileView.swift
@@ -23,24 +23,7 @@ struct ProfileView: View {
 
         //                PostGridView(config: .profile(user))
 
-        HStack(spacing: 24) {
-          NavigationLink(value: CurrentUserProfileNavigation.trainingSessions) {
-            UserStatView(value: String(viewModel.user.stats?.trainingSessions ?? 0), title: "Workouts")
-          }
-          .disabled(viewModel.user.stats?.trainingSessions == 0)
-
-          NavigationLink(value: CurrentUserProfileNavigation.followers(viewModel.user.id)) {
-            UserStatView(value: String(viewModel.user.stats?.followers ?? 0), title: "Followers")
-          }
-          .disabled(viewModel.user.stats?.followers == 0)
-
-          NavigationLink(value: CurrentUserProfileNavigation.following(viewModel.user.id)) {
-            UserStatView(value: String(viewModel.user.stats?.following ?? 0), title: "Following")
-          }
-          .disabled(viewModel.user.stats?.following == 0)
-        }
-        .padding(.trailing)
-        .foregroundColor(.primary)
+        UserStatsHStackView(viewModel: viewModel)
       }
       .padding(.top)
     }
